a7958b85acd4f13ef2004287780f0f0eb49b6622
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-15 00:37:01 +0300

    Refactor N queens puzzle code for improved readability and maintainability

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python3
-"""
-N queens
-"""
+"""N queens puzzle"""
+
 
 import sys
 
+
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     exit(1)
 
 try:
-    NQ = int(sys.argv[1])
+    N = int(sys.argv[1])
 except ValueError:
-    print('N must ba a number')
+    print("N must be a number")
     exit(1)
 
-if NQ < 4:
-    print('N must ba at least 4')
+if N < 4:
+    print("N must be at least 4")
     exit(1)
 
 
-def solve_nqueens(n):
-    ''' QUEENS PUZZLE descriptive '''
-    if n == 0:
+def nQueen(num):
+    """Solves the N queens puzzle"""
+    if num == 0:
         return [[]]
-    inner_solution = solve_nqueens(n - 1)
+    queens_solution = nQueen(num - 1)
     return [solution + [(n, i + 1)]
-            for i in range(NQ)
-            for solution in inner_solution
-            if isSafequeen((n, i + 1), solution)]
+            for i in range(N)
+            for n in range(1, N + 1)
+            for solution in queens_solution
+            if isSafe((n, i + 1), solution)]
 
 
-def attack_queen(square, queen):
-    '''QUEEN Puzzle aelf descriptive'''
+def queen_attack(square, queen):
+    """Check if the square is under attack by a queen"""
     (row1, col1) = square
     (row2, col2) = queen
     return (row1 == row2) or (col1 == col2) or\
         abs(row1 - row2) == abs(col1 - col2)
 
 
-def isSafequeen(sqr, queens):
-    '''self descriptive Nqueens Puzzzle'''
-    for queen in queens:
-        if attack_queen(sqr, queen):
+def isSafe(sq, Qs):
+    """Check if a square is safe from attack by all queens"""
+    for q in Qs:
+        if queen_attack(sq, q):
             return False
     return True
 
 
-for answer in reversed(solve_nqueens(n_q)):
-    Chess_result = []
+for answer in reversed(isSafe(N)):
+    result = []
     for p in [list(p) for p in answer]:
-        Chess_result.append([i - 1 for i in p])
-    print(Chess_result)
+        result.append([i - 1 for i in p])
+    print(result)
