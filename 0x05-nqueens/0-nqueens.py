5b5395127f5dc7bdfc985889d90d9bf8dfb653d1
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-16 18:00:43 +0300

    Refactor N queens puzzle code for improved readability and maintainability

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,64 @@
 #!/usr/bin/python3
-"""N queens puzzle"""
-
-
+"""
+solves the N queens puzzle
+"""
 import sys
 
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
-def nQueen(num):
-    """Solves the N queens puzzle"""
-    if num == 0:
-        return [[]]
-    queens_solution = nQueen(num - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(N)
-            for n in range(1, N + 1)
-            for solution in queens_solution
-            if isSafe((n, i + 1), solution)]
-
-
-def queen_attack(square, queen):
-    """Check if the square is under attack by a queen"""
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
-
-
-def isSafe(sq, Qs):
-    """Check if a square is safe from attack by all queens"""
-    for q in Qs:
-        if queen_attack(sq, q):
+def valid_pos(solu, position):
+    """
+    Function that verifies if a queen can be placed at given
+    position on the chess board with out conflicting with the
+    already placed queens in the list.
+    """
+    for queen in solu:
+        if queen[1] == position[1]:
+            return False
+        if (queen[0] + queen[1]) == (position[0] + position[1]):
+            return False
+        if (queen[0] - queen[1]) == (position[0] - position[1]):
             return False
     return True
 
 
-for answer in reversed(isSafe(N)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+def solve_queens(row, n, solu):
+    """
+    Function that finds the solution recursively, from the root down
+    """
+    if (row == n):
+        print(solu)
+    else:
+        for col in range(n):
+            position = [row, col]
+            if valid_pos(solu, position):
+                solu.append(position)
+                solve_queens(row + 1, n, solu)
+                solu.remove(position)
+
+
+def main(n):
+    """
+    Main function
+    """
+    solution = []
+    """ From root(0) down(n) """
+    solve_queens(0, n, solution)
+
+
+if __name__ == '__main__':
+    """ Validate the arguments from OS """
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+    try:
+        i = int(sys.argv[1])
+    except BaseException:
+        print('N must be a number')
+        sys.exit(1)
+    i = int(sys.argv[1])
+    if i < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    """ Calling the main function """
+    main(i)
