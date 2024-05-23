2ac4b6d212c885ba9f273e9cfdf34dd182057bee
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-24 00:54:32 +0300

    feat: Add script to retrieve Star Wars characters from API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const my_util = require('util');
+const m_request = my_util.promisify(require('request'));
+const filmID = process.argv[2];
+
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
+}
+
+starwarsCharacters(filmID);
